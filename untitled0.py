8b45ed74a4af7480e5ab69906b76d0b7ad69a93a
hysamah <57324118+hysamah@users.noreply.github.com>
2019-12-10 21:28:44 +0200

    Create untitled0.py

---
+++ untitled0.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 10 21:23:04 2019
+
+@author: Dell
+"""
+
+from instructionUnit import *
+
+line = "inst.txt"
+listt = insrtuctionUnit(line)
+i=listt.getIssue(2)
+#listt.revIssue(2)
+p=listt.getIssue(1)

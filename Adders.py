bf1783f9e23a83d189705f4fb6ab71e40b10f032
amroadel <amroadel@aucegypt.edu>
2019-12-10 21:51:52 +0200

    Add files via upload
    Functional units for adders and multipliers

---
+++ Adders.py
@@ -0,0 +1,8 @@
+class Adders: 
+   def __init__(self, result, operand1, operand2):
+       self.result = 0
+       self.operand1 = 0 
+       self.operand2= 0
+    def add(self, result, operand1, operand2):
+        self.result = self.operand1+ self.operand2
+        return self.result 

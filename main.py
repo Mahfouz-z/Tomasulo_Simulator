955ed532327a87f265e0adef79c2dce28a03d945
Mahfouz-z <43938352+Mahfouz-z@users.noreply.github.com>
2019-12-10 22:07:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ dataMemInitFilePath = input("Please input data memorey init file path:")
 
 dataMem0 = dataMem(dataMemInitFilePath)
 instQueue0 = insrtuctionUnit(assmFilePath)
+ROB = ROB(8)
 
+while True:
+     

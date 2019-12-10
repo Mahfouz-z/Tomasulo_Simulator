99f9ac179b28edfa38fb93153e4e6cdb2afdf40d
Mahfouz-z <43938352+Mahfouz-z@users.noreply.github.com>
2019-12-10 20:52:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,19 @@ dataMemInitFilePath = input("Please input data memorey init file path:")
 
 dataMem0 = dataMem(dataMemInitFilePath)
 instQueue0 = insrtuctionUnit(assmFilePath)
-ROB = ROB(8)
+ROB0 = ROB(8)
+RS0 = RS() 
 
+clk = 0
+inst_addr = 0
+numberOfIssues = 2
 while True:
+    for i in range(numberOfIssues):
+        issue = instQueue0.getIssue(1, inst_addr)
+        instType = issue[0].instType
+        if(RS0.checkAvailable(instType)):
+            if(ROB0.check_available() >= 1):
+                
+
+
      

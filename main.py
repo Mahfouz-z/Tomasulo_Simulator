e683c7c82735f4cd3acdf827468be5b9ec8aa2b7
IsmailShaheen <isen.shaheen@outlook.com>
2019-12-12 04:23:12 +0200

    sw modified

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ from NAND import NAND
 #from SW import *
 
 #assmFilePath = input("Please enter assembly file path:")
-dataMemInitFilePath = input("Please input data memorey init file path:")
+#dataMemInitFilePath = input("Please input data memorey init file path:")
 
-dataMem0 = dataMem(dataMemInitFilePath)
+dataMem0 = dataMem("")#dataMemInitFilePath)
 #instQueue0 = insrtuctionUnit(assmFilePath)
 
 instQueue0 = insrtuctionUnit("test.txt")
@@ -41,7 +41,6 @@ reg={}
 for i in range(8):
     reg["x"+ str(i)]=RegFile(None, i)
 
-vj =0
 clk = 0
 pc = 0
 nextPC = []
@@ -82,7 +81,7 @@ while (clk<50):
                 reg[rd].ROBNumber=-1
                 ROB0.remove_entry()
             elif robType == "sw":    
-                dataMem0.update(wordAdd, vj)
+                dataMem0.update(wordAdd, commit["Value"])
                 ROB0.remove_entry()
 
     #simulating execute and writing stage
@@ -92,7 +91,7 @@ while (clk<50):
         if(RS0.get_status(i) == "done"):
             robIndex = RS0.getTargetRob(i)
             instType = RS0.get_type(i)
-            if (instType == "jmp" or instType == "jalr" or instType == "ret"):
+            if (instType == "jmp" or instType == "jalr" or instType == "ret"): #TODO jalr doesn't write to a register
                 nextPCJ.append(result)
             if instType == "beq":
                 beqNum += 1
@@ -105,11 +104,11 @@ while (clk<50):
                 ROB0.upd_entry(robIndex,dataMem0.getData(wordAdd))
             if instType == "sw":
                 wordADD = result
-                vj = RS0.get_Vj(i)
+                ROB0.upd_entry(robIndex,RS0.get_Vj(i))
+            else:
+                ROB0.upd_entry(robIndex,result)
 
-            ROB0.upd_entry(robIndex,result)
             RS0.updRS(robIndex,result)
-            ROB0.upd_entry(robIndex,result)
             RS0.write(i)
         if(RS0.ready(i)):
             RS0.execute(i, pc)
@@ -136,6 +135,5 @@ while (clk<50):
                     break     
             else:
                 break
-    
     
     clk+=1         
